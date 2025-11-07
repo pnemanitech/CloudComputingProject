772d8e7c7c932c3beaef96062ec55c50e59d766a
pnemanitech <106481869+pnemanitech@users.noreply.github.com>
2025-11-07 00:37:13 -0600

    Change database settings to use PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'image_processing',
+        'USER': 'django',
+        'PASSWORD': 'TempleProject10',
+        'HOST': 'image-processing-db.cyzu2goi8ee4.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
